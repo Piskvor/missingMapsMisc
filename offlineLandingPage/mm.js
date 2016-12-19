46b87be697f1d6f092b6b83d2927e2b35e0b2a25
Jan Piskvor Martinec <github@piskvor.org>
2016-12-19 23:46:32 +0100

    get more data from JSON

--- offlineLandingPage/mm.js
+++ offlineLandingPage/mm.js
@@ -1,10 +1,41 @@
 var rnd = Math.random();
 Offline.options = {
     checks: {
-        image: {url: 'https://piskvor.github.io/missingMapsMisc/offlineLandingPage/josm.png?_' + rnd},
+        image: {
+            url: 'https://piskvor.github.io/missingMapsMisc/offlineLandingPage/josm.png?_' + rnd
+        },
         active: 'image'
     }
 };
+
+if (0) {
+// dummy structure for parsing
+    var data = {
+        "current": 2398, // task to do if there's no mapathon currently running
+        "currentMapathon": {
+            "projects": {
+                "basic": { // editing with iD
+                    "id": 2398, // project ID
+                    "name": "Aweil" // project name
+                },
+                "advanced": { // editing with JOSM
+                    "id": 2359,
+                    "name": "Haiti - Port au Prince"
+                }
+            },
+            "name": "Giving Tuesday", // mapathon name
+            "start": "2016-12-16 18:00:00", // start datetime in ISO format
+            "location": { // mapathon location
+                "name": "Paralelní police",
+                "address": "U úlů 1234, Praha",
+                "lat": "50.12249",
+                "lon": "14.63599",
+                "zoom": null, // suggested zoom to show, null to choose automatically
+                "link": "https://openstreetmap.cz/way/8072073" // link to provide
+            }
+        }
+    };
+}
 
 var doCheckTask = function ($) {
     $('.mm-is-offline').hide();
@@ -15,16 +46,33 @@ var doCheckTask = function ($) {
         dataType: 'json',
         cache: false,
         timeout: 30000,
-        success: function (data, status, xhr) {
+        success: function (data) {
             $('.mm-is-online-checking').hide();
             $('.mm-is-online-done').show();
-            $('.mm-link').each(function (idx, xlink) {
-                var link = $(xlink);
-                link.attr('href', link.data('href').replace('__ID__', data.current));
-                link.text(link.text().replace('__ID__', data.current));
-            });
+            if (data.currentMapathon) {
+                var loopBy = ['basic','advanced'];
+                for (var i = loopBy.length - 1; loopBy.length >= 0; i--) {
+                    if (data.currentMapathon.projects[loopBy[i]]) {
+                        var project = data.currentMapathon.projects[loopBy[i]];
+                        $('.mm-' + loopBy[i] + ' .mm-link').each(function (idx, xlink) {
+                            var link = $(xlink);
+                            link.attr('href', link.data('href').replace('__ID__', project.id));
+                            link.text(link.text().replace('__ID__', project.id).replace('__NAME__', project.name));
+                        });
+                    } else {
+                        $('.mm-' + loopBy[i]).hide();
+                    }
+                }
+            } else {
+                $('.mm-advanced').hide();
+                $('.mm-link').each(function (idx, xlink) {
+                    var link = $(xlink);
+                    link.attr('href', link.data('href').replace('__ID__', data.current));
+                    link.text(link.text().replace('__ID__', data.current));
+                });
+            }
         },
-        error: function (xhr, errorType, error) {
+        error: function () {
             Offline.check();
         }
     })
