740ba530856b18b04c67cc25c4fa63bdcbba4fe9
Jan Piskvor Martinec <github@piskvor.org>
2016-12-20 14:53:56 +0100

    comments and formatting

--- Open_in_JOSM_webextension/background.js
+++ Open_in_JOSM_webextension/background.js
@@ -1,51 +1,57 @@
 // actually sends the request out - by opening a new tab
 function osm_piskvor_org_share(e) {
+    // we need to hardcode this, JOSM doesn't let us configure
     const josmURL = "http://localhost:8111/import?url=";
+
     let url;
-    if (typeof(e['target']) == 'undefined') {
+    if (typeof(e["target"]) == "undefined") {
         url = e.toString();
     } else {
+        // in case we get passed an event
         url = e.target.getAttribute("url");
     }
-    const hasHash = url.indexOf('#');
+    const hasHash = url.indexOf("#");
+    // if hash, try to convert
     if (hasHash !== -1) {
         const hash = url.substring(hasHash + 1);
         url = url.substring(0, hasHash);
-        if (hash.indexOf('task') === 0) { // special handling for project tasks on tasks.hotosm.org
-            url += '/' + hash + '.gpx';
+        if (hash.indexOf("task") === 0) { // special handling for project tasks on tasks.hotosm.org
+            url += "/" + hash + ".gpx";
         }
     }
-    let loadUrl;
+    let loadUrl = url;
     if (url.indexOf(josmURL) === -1) { // 80/20 try to avoid loops - calling /loadUrl=http://localhost/loadUrl
         loadUrl = josmURL + encodeURIComponent(url);
-    } else {
-        loadUrl = url;
     }
-    // background tab
+    // open in background tab
     browser.tabs.create({
         url: loadUrl,
         active: false
     });
 }
 
-
+// create the context menu item
 browser.contextMenus.create({
     id: "osm-piskvor-org-josm-remote-link",
     title: browser.i18n.getMessage("contextMenuItemOpenInJosm"),
-    contexts: ["selection","link","page"]
+    contexts: ["selection", "link", "page"]
 });
 
-/*
- The click event listener, where we perform the appropriate action given the
- ID of the menu item that was clicked.
- */
+// handle context menu click
 browser.contextMenus.onClicked.addListener(function (info) {
-    if (info['selectionText']) {
+    // we only care about our one item
+    if (info.menuItemId != "osm-piskvor-org-josm-remote-link") {
+        return;
+    }
+    if (info["selectionText"]) {
+        // if there's a selection, load that
         osm_piskvor_org_share(info.selectionText);
-    } else if (info['linkUrl']) {
+    } else if (info["linkUrl"]) {
+        // if there's a link, load that
         osm_piskvor_org_share(info.linkUrl);
     } else {
-        osm_piskvor_org_share(info.frameUrl);
+        // otherwise load the current page URL
+        osm_piskvor_org_share(info.pageUrl);
     }
 });
 
