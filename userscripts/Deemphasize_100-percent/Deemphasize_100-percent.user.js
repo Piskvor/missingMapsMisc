c8334c72ef421fc75173bad05c673a89fcdd78d5
Jan Piskvor Martinec <github@piskvor.org>
2017-01-05 12:41:26 +0100

    formatting

--- userscripts/Deemphasize_100-percent/Deemphasize_100-percent.user.js
+++ userscripts/Deemphasize_100-percent/Deemphasize_100-percent.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Deemphasize complete projects
 // @namespace    http://osm.piskvor.org/
-// @version      0.3
+// @version      0.3.1
 // @description  Don't clutter project list with completed ones (100%)
 // @author       Piskvor
 // @license      WTFPL http://sam.zoy.org/wtfpl/COPYING
@@ -11,38 +11,40 @@
 // @updateURL    https://piskvor.github.io/missingMapsMisc/userscripts/Deemphasize_100-percent/Deemphasize_100-percent.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    var addGlobalStyle = function(css) {
+    var addGlobalStyle = function (css) {
         var head, style;
         head = document.getElementsByTagName('head')[0];
-        if (!head) { return; }
+        if (!head) { // A headless knight! (Or it's not HTML)
+            return;
+        }
         style = document.createElement('style');
         style.type = 'text/css';
         style.innerHTML = css;
         head.appendChild(style);
     };
 
-    var deemphasize = function($percent, pct) {
-        //noinspection EqualityComparisonWithCoercionJS
+    var deemphasize = function ($percent, pct) {
+        //noinspection EqualityComparisonWithCoercionJS - we do want type coercion here
         if (pct == 100) {
             var $project = $percent.closest(".project");
-            if ($project.length) {
+            if ($project.length) { // project exists
                 $project.children().not(".project-stats").not('.clear,h4').hide();
                 $project.addClass("deemphasize");
             }
         }
     };
 
-    jQuery(function($){
+    jQuery(function ($) { // run when all loaded
         addGlobalStyle('.deemphasize { opacity: 0.5 }');
 
-        $(".project-stats .progress").each(function(idx,elem){
-            var $pct=$(elem.parentElement.parentElement).find('td:last');
+        $(".project-stats .progress").each(function (idx, elem) { // since we climb up and down the tree, we can't just use a selector
+            var $pct = $(elem.parentElement.parentElement).find('td:last');
             var pct = $pct.text();
-            if(pct.indexOf('%')>= 0){
-                deemphasize($pct, pct.replace(' ','').replace('%', ''));
+            if (pct.indexOf('%') >= 0) { // we have a percentage
+                deemphasize($pct, pct.replace(' ', '').replace('%', ''));
             }
         });
     });
