35b1a987eb15f99cffb4d5f315a432f2b433e126
Jan Piskvor Martinec <github@piskvor.org>
2017-01-13 22:02:16 +0100

    serve-ff

--- puppy-live/my-applications/Startup/serve-ff.sh
+++ puppy-live/my-applications/Startup/serve-ff.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /root/my-applications/bin/spot /root/my-applications/bin/serve-local-www.sh &
-sleep 3
+sleep 2
 /root/my-applications/bin/spot /root/my-applications/bin/get-josm-user.sh
+sleep 1
 /root/my-applications/bin/foxspot
