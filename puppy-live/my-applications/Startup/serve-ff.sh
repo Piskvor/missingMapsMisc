409aea808e60d259f590ce424acdb7a99fab1903
Jan Piskvor Martinec <github@piskvor.org>
2017-01-13 14:01:54 +0100

    add startup scripts

---
+++ puppy-live/my-applications/Startup/serve-ff.sh
@@ -0,0 +1,4 @@
+
+/root/my-application/bin/spot /root/my-applications/bin/serve-local-www.sh &
+sleep 5
+/root/my-applications/bin/foxspot
--- puppy-live/my-applications/bin/serve-local-www.sh
+++ puppy-live/my-applications/bin/serve-local-www.sh
@@ -4,5 +4,5 @@ cd /root/Web-Server
 if [ "$1" = "--debug" ]; then 
 	/usr/bin/python /root/my-applications/bin/serve-local.py
 else
-	/usr/bin/python /root/my-applications/bin/serve-local.py >/dev/null 2>/dev/null
+	/usr/bin/python /root/my-applications/bin/serve-local.py >/dev/null 2>/dev/null 
 fi
