edf0e470a9a9a9faa3bf0140ccfecbd74785e037
Jan Piskvor Martinec <github@piskvor.org>
2017-01-13 17:58:02 +0100

    check JOSM when pulling

--- puppy-live/my-applications/bin/update-josm-latest.sh
+++ puppy-live/my-applications/bin/update-josm-latest.sh
@@ -5,5 +5,7 @@ wget --timestamping https://josm.openstreetmap.de/josm-tested.jar
 wget --timestamping https://josm.openstreetmap.de/josm-latest.jar
 cd $HOME/my-documents/missingMapsMisc
 git pull
+/root/my-applications/bin/spot /root/my-applications/bin/get-josm-user.sh
+echo "Done."
 exit $?
 
